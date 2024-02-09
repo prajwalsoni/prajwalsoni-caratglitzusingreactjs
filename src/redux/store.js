52537e4515fef88110016da3825ca7b3c378fb89
prajwalsoni <prajwalsonics@gmail.com>
2024-02-09 18:38:06 +0530

    Updated Final Commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,7 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import orebiReducer from "./soniSlice";
+import soniReducer from "./soniSlice";
 
 const persistConfig = {
   key: "root",
@@ -18,7 +18,7 @@ const persistConfig = {
   storage,
 };
 
-const persistedReducer = persistReducer(persistConfig, orebiReducer);
+const persistedReducer = persistReducer(persistConfig, soniReducer);
 
 export const store = configureStore({
   reducer: { orebiReducer: persistedReducer },
